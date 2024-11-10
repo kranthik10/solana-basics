6d1262abc1237205a3f0c517dd035dfd05d790d8
kranthi11222 <kk22ace@herts.ac.uk>
2024-11-10 20:01:01 +0000

    generation of keypairs

---
+++ getKey.ts
@@ -0,0 +1,9 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import { getKeypairFromEnvironment } from '@solana-developers/helpers'
+
+
+const keypair = getKeypairFromEnvironment('SECRET_KEY');
+
+console.log('Success!');
