7719c1e751600875cd315d8082c77fd250e87e9d
kranthi11222 <kk22ace@herts.ac.uk>
2024-11-10 20:05:40 +0000

    no change just organised a bit

--- getKey.ts
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
-import { getKeypairFromEnvironment } from '@solana-developers/helpers'
-
-
-const keypair = getKeypairFromEnvironment('SECRET_KEY');
-
-console.log('Success!');
