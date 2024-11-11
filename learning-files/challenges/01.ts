c09037bb71f330aeb0b930f14a22b09caf2e950c
kranthi11222 <kk22ace@herts.ac.uk>
2024-11-11 00:01:16 +0000

    check for valid address

--- learning-files/challenges/01.ts
+++ learning-files/challenges/01.ts
@@ -7,34 +7,61 @@
 // todo need to add the code for handling invalid wallet addresses
 import { Connection, PublicKey,LAMPORTS_PER_SOL} from '@solana/web3.js';
 
-
 import { getDomainKeySync } from "@bonfida/spl-name-service";
 
 const domainName = "shaq.sol";
 
 const connection = new Connection('https://api.mainnet-beta.solana.com');
 
-
 const { pubkey: domainPubkey } = getDomainKeySync(domainName);
 
-console.log(domainPubkey.toBase58());
+console.log(domainPubkey);
 
 
 
 
-// const givenPublicKey = process.argv[2]; // argument passed from the command line
+function isValidSolanaAddress(address:string) {
+    try {
+        // Attempt to create a PublicKey from the given address.
+        // If successful, the address is valid.
+        new PublicKey(address);
+        return true;
+    } catch (error) {
+        // If an error is thrown, the address is invalid.
+        return false;
+    }
+}
 
-// if(!givenPublicKey){
-//     console.log('Please provide a public key');
-//     process.exit(1);
-// }
 
 
- const publicKey = new PublicKey(domainPubkey);
 
-const  balance = await connection.getBalance(publicKey);
+const givenPublicKey = process.argv[2]; // argument passed from the command line
 
-const balanceInSol = balance / LAMPORTS_PER_SOL;
+if(!givenPublicKey){
+    console.log('Please provide a public key');
+    process.exit(1);
+}
 
- console.log(balanceInSol);
+
+if(!isValidSolanaAddress(givenPublicKey)){
+    console.log('Please provide a valid public key');
+    process.exit(1);
+}
+
+
+
+const publicKey = new PublicKey(domainPubkey);
+
+const  getBalance =  async () => {
+    try{
+        const balance = await connection.getBalance(publicKey);
+        const balanceInSol = balance / LAMPORTS_PER_SOL 
+
+    }
+    catch(error){
+        console.log('Error:', error);
+    }
+}
+
+
 
